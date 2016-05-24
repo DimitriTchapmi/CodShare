512f2f8bc2c80c8711c99818bf4298cd3a82e3e2
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-05-24 15:11:45 +0200

    ajout script delete_fqdn

--- scripts/addfqdn.sh
+++ scripts/addfqdn.sh
@@ -5,7 +5,8 @@ if sudo grep -q =$nomuser.codshare /etc/tinydns/root/data; then
 	else 
 		sudo echo =$nomuser.codshare.itinet.fr:88.177.168.133:86400 >> /etc/tinydns/root/data
 fi
-
+cd /etc/tinydns/root/
+sudo make
 
 
 
