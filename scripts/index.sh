2701cfe7610ab9f58ce7ea12e89de714552ff87f
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-05-24 13:04:23 +0200

    fixation scripts web

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
+nomuser=$1
+	mkdir /var/www/codshare/$nomuser
+	touch /var/www/codshare/$nomuser/index.html
 
-	mkdir /var/www/codshare/$1
-	touch /var/www/codshare/$1/index.html
-
-	echo "<!DOCTYPE html>" >> /var/www/codshare/$1/index.html
-	echo "<html>" >> /var/www/codshare/$1/index.html
-	echo "<head>" >> /var/www/codshare/$1/index.html
-	echo "<title>$1</title>" >> /var/www/codshare/$1/index.html
-	echo "</head>" >> /var/www/codshare/$1/index.html
-	echo "<body>" >> /var/www/codshare/$1/index.html
-	echo "<h1>$1</h1>" >> /var/www/codshare/$1/index.html
-	echo "</body>" >> /var/www/codshare/$1/index.html
-	echo "</html>" >> /var/www/codshare/$1/index.html
+	echo "<!DOCTYPE html>" >> /var/www/codshare/$nomuser/index.html
+	echo "<html>" >> /var/www/codshare/$nomuser/index.html
+	echo "<head>" >> /var/www/codshare/$nomuser/index.html
+	echo "<title>$nomuser</title>" >> /var/www/codshare/$nomuser/index.html
+	echo "</head>" >> /var/www/codshare/$nomuser/index.html
+	echo "<body>" >> /var/www/codshare/$nomuser/index.html
+	echo "<h1>$nomuser</h1>" >> /var/www/codshare/$nomuser/index.html
+	echo "</body>" >> /var/www/codshare/$nomuser/index.html
+	echo "</html>" >> /var/www/codshare/$nomuser/index.html
