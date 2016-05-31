8f5712ddb7071d701fee7ffcbe7ce694e8e39842
Baptiste Beugnet <beugnet@intechinfo.fr>
2016-05-31 19:41:35 +0200

    modification script user owncloud 3

--- scripts/add_user_owncloud.sh
+++ scripts/add_user_owncloud.sh
@@ -4,4 +4,4 @@ PASS=$2
 group=$3
 
 export OC_PASS=$2
-su -s /bin/sh www-data -c 'php occ user:add --password-from-env --display-name=$1 --group=$3 $1
+su -s /bin/sh www-data -c php occ user:add --password-from-env --display-name=$1 --group=$3 $1
