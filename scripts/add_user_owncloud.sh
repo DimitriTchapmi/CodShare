4273e2a182e114e8da333234beb5acf001c2dd97
Baptiste Beugnet <beugnet@intechinfo.fr>
2016-06-01 10:28:24 +0200

    modification script ajout user owncloud 16

--- scripts/add_user_owncloud.sh
+++ scripts/add_user_owncloud.sh
@@ -3,6 +3,6 @@ user=$1
 newpassword=$2
 group=$3
 cd /var/www/owncloud/
-sudo -u www-data php occ user:add --display-name="$user" --group="$group" $user
->$newpassword
->$newpassword 
+sudo -u www-data php occ --no-interaction user:add --display-name="$user" --group="$group" $user 
+$newpassword | echo"$newpassword"
+$newpassword | echo"$newpassword"
