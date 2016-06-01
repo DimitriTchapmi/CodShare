af60b34e63bfa8e1259f9503e8436a16c5c7e4c0
Baptiste Beugnet <beugnet@intechinfo.fr>
2016-06-01 09:32:56 +0200

    modification script ajout user owncloud 10

--- scripts/add_user_owncloud.sh
+++ scripts/add_user_owncloud.sh
@@ -3,5 +3,4 @@ user=$1
 newpassword=$2
 group=$3
 cd /var/www/owncloud/
-export OC_PASS=$newpassword
-su -s /bin/sh www-data -c 'php occ user:add [--password-from-env] --display-name="$1" --group="$3" $1'
+su -s /bin/sh www-data -c 'php occ user:add --password-from-env=$2 --display-name="$1" --group="$3" $1'
