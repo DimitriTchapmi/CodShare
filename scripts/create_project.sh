d0eb47c47587dfbb3b15e8bd587a2a8eb8648f6e
Baptiste Beugnet <beugnet@intechinfo.fr>
2016-06-02 10:38:53 +0200

    modification crea

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -33,7 +33,7 @@ sudo postfix reload
 
 sudo echo "####Création du dossier Cloud du projet####"
 cd /var/www/owncloud
-sudo ./add_user_owncloud.sh $nom_user $mdp_projet $nom_projet
+./add_user_owncloud.sh $nom_user $mdp_projet $nom_projet
 
 sudo echo "####Création du Depôt Git du projet####"
 sudo mkdir $depot_git$nom_projet
