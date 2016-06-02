cd25d5a10615c349d023943dcfe9942a1a88e746
Baptiste Beugnet <beugnet@intechinfo.fr>
2016-06-02 09:26:02 +0200

    modification document root vhost

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 nomuser=$1
-	mkdir /var/www/$nomuser
-	touch /var/www/$nomuser/index.html
+	mkdir /var/www/site/$nomuser
+	touch /var/www/site/$nomuser/index.html
 
-	echo "<!DOCTYPE html>" >> /var/www/$nomuser/index.html
-	echo "<html>" >> /var/www/$nomuser/index.html
-	echo "<head>" >> /var/www/$nomuser/index.html
-	echo "<title>$nomuser</title>" >> /var/www/$nomuser/index.html
-	echo "</head>" >> /var/www/$nomuser/index.html
-	echo "<body>" >> /var/www/$nomuser/index.html
-	echo "<h1>$nomuser</h1>" >> /var/www/$nomuser/index.html
-	echo "</body>" >> /var/www/$nomuser/index.html
-	echo "</html>" >> /var/www/$nomuser/index.html
+	echo "<!DOCTYPE html>" >> /var/www/$nomuser/site/index.html
+	echo "<html>" >> /var/www/$nomuser/site/index.html
+	echo "<head>" >> /var/www/$nomuser/site/index.html
+	echo "<title>$nomuser</title>" >> /var/www/$nomuser/site/index.html
+	echo "</head>" >> /var/www/$nomuser/site/index.html
+	echo "<body>" >> /var/www/$nomuser/site/index.html
+	echo "<h1>$nomuser</h1>" >> /var/www/$nomuser/site/index.html
+	echo "</body>" >> /var/www/$nomuser/site/index.html
+	echo "</html>" >> /var/www/$nomuser/site/index.html
