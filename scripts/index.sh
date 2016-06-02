d14d2bb34e041fcd86785ba58ca6b866a0950e81
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-02 07:27:39 +0200

    restructuration des codes + script inscription

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 nomuser=$1
-	mkdir /var/www/$nomuser
-	touch /var/www/$nomuser/index.html
+depot_site="/var/www/site/"
+	mkdir $depot_site$nomuser
+	touch $depot_site$nomuser/index.html
 
-	echo "<!DOCTYPE html>" >> /var/www/$nomuser/index.html
-	echo "<html>" >> /var/www/$nomuser/index.html
-	echo "<head>" >> /var/www/$nomuser/index.html
-	echo "<title>$nomuser</title>" >> /var/www/$nomuser/index.html
-	echo "</head>" >> /var/www/$nomuser/index.html
-	echo "<body>" >> /var/www/$nomuser/index.html
-	echo "<h1>$nomuser</h1>" >> /var/www/$nomuser/index.html
-	echo "</body>" >> /var/www/$nomuser/index.html
-	echo "</html>" >> /var/www/$nomuser/index.html
+	echo "<!DOCTYPE html>" >> $depot_site$nomuser/index.html
+	echo "<html>" >> $depot_site$nomuser/index.html
+	echo "<head>" >> $depot_site$nomuser/index.html
+	echo "<title>$nomuser</title>" >> $depot_site$nomuser/index.html
+	echo "</head>" >> $depot_site$nomuser/index.html
+	echo "<body>" >> $depot_site$nomuser/index.html
+	echo "<h1>$nomuser</h1>" >> $depot_site$nomuser/index.html
+	echo "</body>" >> $depot_site$nomuser/index.html
+	echo "</html>" >> $depot_site$nomuser/index.html
--- scripts/vhost.sh
+++ scripts/vhost.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 nomuser=$1
+depot_site="/var/www/site/"
 touch etc/apache2/sites-available/$nomuser.conf
 echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$nomuser.conf
 echo "ServerName $nomuser.codshare.itinet.fr" >> /etc/apache2/sites-available/$nomuser.conf
 echo "ServerAlias www.$nomuser.codshare.itinet.fr" >> /etc/apache2/sites-available/$nomuser.conf
-echo "DocumentRoot /var/www/$nomuser" >> /etc/apache2/sites-available/$nomuser.conf
+echo "DocumentRoot $depot_site$nomuser" >> /etc/apache2/sites-available/$nomuser.conf
 echo "</VirtualHost>" >> /etc/apache2/sites-available/$nomuser.conf
 a2ensite $nomuser.conf
 service apache2 reload 
