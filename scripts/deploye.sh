a1ada135b1e4791819fe5b56e4543081198e0601
baba <baba@codshare.itinet.fr>
2016-06-03 12:20:58 +0200

    script de deploiement

--- scripts/deploye.sh
+++ scripts/deploye.sh
@@ -3,14 +3,14 @@
 depot_git="/var/www/git"
 depot_site="/var/www/site/" 
 
-if [ -f /$depot_site$nom_projet ]
+if [ -f $depot_site$nom_projet ]
 	then
-	" le fichier existe deja "
+	echo " le fichier existe deja "
 	git pull 
 else 
 	echo " Creation du fichier en cours... "
 	cd $depot_site
-	git clone precieux@10.8.96.3:/$depot_git$nom_projet
+	git clone $nomuser@10.8.96.3:/$depot_git$nom_projet
 	echo " Le fichier a bien ete cree "
 fi
 
