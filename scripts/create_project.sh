7284fa8b21b4a6d09e5ac138a87fca3b39890d24
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-07 15:41:19 +0200

    modif script avec keys dev

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -2,18 +2,28 @@
 
 depot_git="/var/www/git/"
 depot_site="/var/www/site/"
+dossier_keys="/home/codshare-itinet/"
 
 nom_user=$1
 nom_projet=$2
+mdp_user=$3
 
+###Création du compte Unix du chef de projet###
+if grep -q ^$nom_user /etc/passwd; then
+	sudo echo "User $nom_user already exist in /etc/passwd !"
+else
+	sudo useradd -p $mdp_user -d /home/$nom_user -s /usr/bin/mysecureshell $nom_user 
+	#changing to encrypted password
+    sudo echo "$nom_user:$mdp_user" | sudo chpasswd
+fi
 
 sudo echo "####Création du groupe Unix pour le projet####"
-sudo addgroup $nom_projet
+sudo useradd --create-home $nom_projet
 
+sudo echo "###Création du depot_site du projet###"
 sudo mkdir $depot_site$nom_user
+chmod -R 750 $depot_site$nom_user
 sudo chown -R $nom_user:www-data $depot_site$nom_user
-
-sudo usermod -G $nom_projet $nom_user
 
 sudo echo "####Création de l'Alias mail du projet####"
 sudo echo "$nom_projet@codshare.itinet.fr $nom_user@codshare.itinet.fr" >> /etc/postfix/virtual
@@ -29,7 +39,12 @@ sudo echo "####Création du Depôt Git du projet####"
 sudo mkdir $depot_git$nom_projet
 cd $depot_git$nom_projet
 git init --bare
+sudo echo ""
 chmod -R 770 $depot_git$nom_projet
 chown -R $nom_user:$nom_projet $depot_git$nom_projet
+
+#Ajout chef key to authorized keys
+
+sudo echo $dossier_keys$nom_user > /home/$nom_projet/.ssh/authorized_keys
 
 
--- scripts/inscription.sh
+++ scripts/inscription.sh
@@ -5,16 +5,9 @@ nom_user=$1
 mdp_user=$2
 verification=`sudo grep $nom_user /etc/postfix/vmailbox`
 cheminUtil=/$nom_user/
+dossier_keys="/home/codshare-itinet/"
 
-#Création du compte Unix du développeur
-if grep -q ^$nom_user /etc/passwd; then
-	sudo echo "User $nom_user already exist in /etc/passwd !"
-else
-	sudo useradd -p $mdp_user -d /home/$nom_user -s /usr/bin/mysecureshell $nom_user 
-	#changing to encrypted password
-    sudo echo "$nom_user:$mdp_user" | sudo chpasswd
-fi
-#Création de l'utilisateur dans vmailbox :
+sudo echo "###Création de l'utilisateur dans vmailbox..."
 #Si $verification est vide on enregistre l'utilisateur
 if [ -z "$verification" ]; then	
 	sudo echo $nom_user	$cheminUtil >> /etc/postfix/vmailbox
@@ -23,9 +16,8 @@ else
 	sudo echo "Cette adresse mail existe déja"
 	exit
 fi
-
+sudo echo "Création de la boite mail..."
 #Création de son répertoire avec les bons droits :
-
 sudo mkdir /var/mail/$nom_user
 sudo mkdir /var/mail/$nom_user/cur
 sudo mkdir /var/mail/$nom_user/new
@@ -36,7 +28,6 @@ sudo chown -R codsharemail:codsharemail /var/mail/$nom_user
 sudo service postfix reload 
 
 #Configuration IMAP 
-
 #Inscription dans userdb :
 sudo userdb $nom_user set uid=1007 gid=1007 home=/var/mail/$nom_user mail=/var/mail/$nom_user
 
@@ -45,5 +36,12 @@ sudo userdb $nom_user set uid=1007 gid=1007 home=/var/mail/$nom_user mail=/var/m
 
 #Compilation du fichier userdb
 sudo makeuserdb
+
+sudo echo "###Génération des clés ssh de conexion au dépôt git..."
+sudo mkdir $dossier_keys$nom_user/
+cd $dossier_keys$nom_user/
+sudo ssh-keygen -t rsa -b 1024 -f $nom_user
+
+
 
 
