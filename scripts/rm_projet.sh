1772b35379169a78aff5b6ca2a1a7fb3348187ef
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-08 10:37:40 +0200

    super bonne modif

--- scripts/rm_projet.sh
+++ scripts/rm_projet.sh
@@ -1,20 +1,32 @@
 #! /bin/bash
-projet=$1
-utilisateur=$2
-sudo rm -r /var/www/git/$projet
-sudo rm -r /var/www/site/$projet
-sudo a2dissite $projet.conf
+nom_projet=$1
+nom_chef=$2
+###Suppression du depôt Git###
+sudo rm -r /var/www/git/$nom_projet
+
+###Suppression du site###
+sudo rm -r /var/www/site/$nom_projet
+sudo a2dissite $nom_projet.conf
 sudo service apache2 reload
-sudo rm /etc/apache2/sites-avaliable/$projet.conf
-sudo sed -i /"$projet@codshare.itinet.fr"/d /etc/postfix/virtual
-if sudo grep -q =$projet.codshare.itinet.fr /etc/tinydns/root/data; then
-		sudo sed -i /"=$projet.codshare.itinet.fr"/d /etc/tinydns/root/data
+sudo rm /etc/apache2/sites-avaliable/$nom_projet.conf
+
+###Suppression du mail###
+sudo sed -i /"$nom_projet@codshare.itinet.fr"/d /etc/postfix/virtual
+sudo rm -r /var/mail/$nom_projet
+
+###Suppression du fqdn###
+if sudo grep -q =$nom_projet.codshare.itinet.fr /etc/tinydns/root/data; then
+		sudo sed -i /"=$nom_projet.codshare.itinet.fr"/d /etc/tinydns/root/data
 	else
 		sudo echo "Ce fqdn n'existe pas encore !"
 fi
 cd /etc/tinydns/root/
 sudo make
+
+###Suppression du Cloud###
 cd /var/www/owncloud/
-sudo -u www-data php occ user:delete $utilisateur
-sudo groupdel $projet
-sudo sftp-user delete $utilisateur
+sudo -u www-data php occ user:delete $nom_chef
+rm -r /home/dossiercloud/$nom_projet
+
+###Suppression du Sftp###
+sudo sftp-user delete $nom_chef
--- scripts/rm_user_from_project.sh
+++ scripts/rm_user_from_project.sh
@@ -3,7 +3,7 @@ nom_user=$1
 nom_projet=$2
 ###suppression du dev du groupe Unix du projet###
 sudo gpasswd -d $nom_user $nom_projet
-### suppression du mail du dev de l'alias ###
+###suppression du mail du dev de l'alias ###
 sudo  grep ^$nom_projet@codshare.itinet.fr /etc/postfix/virtual | sed -i 's/" $nom_user,"//g' /etc/postfix/virtual
 
 	
