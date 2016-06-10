86ff07bcee177efd29a3535d91fe2a84415d6c27
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-10 15:20:01 +0200

    retouches 3.0

--- scripts/rm_dev_from_project.sh
+++ scripts/rm_dev_from_project.sh
@@ -4,8 +4,7 @@ nom_projet=$2
 dossier_keys="/home/codshare-itinet/"
 
 ### suppression du mail du dev de l'alias ###
-sudo grep ^$nom_projet@codshare.itinet.fr | sed -i 's/'", $nom_user@codshare.itinet.fr"'//g' 
- /etc/postfix/virtual
+sudo grep ^$nom_projet@codshare.itinet.fr | sed -i 's/'", $nom_user@codshare.itinet.fr"'//g' /etc/postfix/virtual
 
 ### suppression de la key_user.pub dans authorized_keys du compte unix projet
 key_user=$(sudo cat $dossier_keys$nom_user/$nom_user.pub)
