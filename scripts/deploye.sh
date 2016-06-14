c5b427591ddd1346f427bf59a2d83adaf0520035
precieux <precieuxnsiemo@hotmail.com>
2016-06-14 09:37:33 +0200

    modif script

--- scripts/deploye.sh
+++ scripts/deploye.sh
@@ -11,12 +11,19 @@ nom_projet=$1
 depot_git="/var/www/git/"
 depot_site="/var/www/site/" 
 
-if [ -f $depot_site$nom_projet ]
+if [ -d $depot_site$nom_projet ]
 	then
 	echo " le fichier existe "
+	cd $depot_git$nom_projet
+	git add .
+	git commit -m "update"
+	cd $depot_site$nom_projet
 	git pull 
 else 
 	echo " Creation du fichier en cours... "
+	cd $depot_git$nom_projet
+	git add .
+	git commit -m "premier commit"
 	cd $depot_site
 	git clone $depot_git$nom_projet
 fi
