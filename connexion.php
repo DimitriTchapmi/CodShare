b02071c8c1cc75290a921e62dd38c40bb23f7de1
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-16 11:51:28 +0200

    modif template

---
+++ connexion.php
@@ -0,0 +1,39 @@
+<?php
+
+echo '
+
+<head>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href=https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700 rel=stylesheet type=text/css>
+
+</head>
+<body>
+<div class="col-lg-12">
+<div class="row">
+<div class="col-md-6">
+<div class="form-group">
+<p class="text-muted"> veuillez vous inscrire pour participer a notre histoire </p>
+<p class="text-muted"> nom : <input type="text" name="nom" class="form-control"></p>
+<p class="text-muted"> prénom : <input type="text" name="penom" class="form-control"></p>
+<p class="text-muted"> mot de passe : <input type="password" name="pass" class="form-control"></p>
+<p class="text-muted"> retapez votre mot de passe : <input type="password" name="passw2" class="form-control"></p>
+<p class="text-muted"> adresse mail : <input type="text" name="mail" class="form-control"></p>
+<input type="button" name="envoyer" value="envoyer" class="btn btn-xl">
+</div>
+</div>
+</div>
+</div>
+</body>';
+
+
+?>
