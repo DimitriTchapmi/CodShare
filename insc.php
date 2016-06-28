a2530bf5c60ad6685d50a0e46ac8711e5bcdf0ba
DimitriTchapmi <tchapmiwagna@intechinfo.fr>
2016-06-28 09:49:49 +0200

    inscription fonctionnelle

--- insc.php
+++ insc.php
@@ -1,13 +1,16 @@
 <?php
 include 'db.php';
-$pass=sha1($_POST["pass"]);
-$req = $bdd->prepare('INSERT INTO `developpeurs`(`nom` ,`prenom` ,`email` ,`login` ,`mdp`, `compte unix_id_unix`) VALUES(:nom, :prenom, :email, :pseudo, :pass, :compte');
+$pass=sha1($_POST['pass']);
+$req = $bdd->prepare('INSERT INTO `compte unix`(`login` ,`mdp`) VALUES(:login, :mdp)');
+$req->execute(array(
+	'login' => $_POST['pseudo'],
+	'mdp' => $pass,
+	)) or die (print_r($req->errorInfo()));
+$req = $bdd->prepare('INSERT INTO `developpeurs`(`nom` ,`prenom` ,`email`, `compte unix_id_unix`) VALUES(:nom, :prenom, :email, :compte)');
 $req->execute(array(
 	'nom' => $_POST['nom'],
 	'prenom' => $_POST['prenom'],
 	'email' => $_POST['email'],
-	'pseudo' => $_POST['pseudo'],
-	'pass' => $pass,
 	'compte'=> '1',
 	)) or die (print_r($req->errorInfo()));
 ?>
