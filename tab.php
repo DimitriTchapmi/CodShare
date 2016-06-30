977da6ff1a5ac08e810531c3f401c03537c155ac
hermann <nsiemo@intechinfo.fr>
2016-07-01 01:07:39 +0200

    precieux

--- tab.php
+++ tab.php
@@ -5,6 +5,7 @@
     <meta charset ="utf-8">
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/agency.css" rel="stylesheet">
@@ -15,6 +16,12 @@
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href=https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700 rel=stylesheet type=text/css>
+
+<!--     Pour le tableau -->
+<!--      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
 
 </head>
 <body bgcolor="#E6E6FA">
@@ -43,17 +50,14 @@
                     <li>
                         <a class="page-scroll" href="http://mail.codshare.itinet.fr">email</a>
                     </li>
-                    <li>
-                        <a class="page-scroll" href="#creer">créer un projet</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="http://localhost/codshare/profil.php">Profil</a>
-                    </li>
-                    <li>
+<!--                     <li>
                         <a class="page-scroll" href="#liste">liste de projet</a>
+                    </li>       -->
+                    <li>
+                        <a class="page-scroll" href="http://localhost/codshare/param.php" title="accédez a votre profil" data-toggle="modal">Welcome</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="http://localhost/codshare/deconnexion.php" data-toggle="modal">déconnexion</a>
+                        <a class="page-scroll" href="http://localhost/codshare/deconnexion.php" data-toggle="modal"> <button type="button" class="btn btn-danger"> déconnexion</button></a>
                     </li>
                     </ul>
             </div>
@@ -62,34 +66,11 @@
         <!-- /.container-fluid -->
     </nav>
 
-   <!-- Header -->
-    <header>
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-lead-in">Welcome $pseudo</div>
-            </div>
-        </div>
-    </header>
 
 <!-- ========================================================================================================================================== -->
-
-    <!-- email Section 
-    <section id="email">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Vous disposez désormais d\'une boite mail</h2>
-                     <a href="http://mail.codshare.itinet.fr"><button type="button" class="btn btn-primary">login@codshare.itinet.fr</button></a> 
-                </div>
-            </div>
-           
-        </div>
-    </section> -->
-
-<!-- ========================================================================================================================================== -->
-
+        
     <!-- creer Section -->
-    <section id="creer" class="bg-light-gray">
+  <!--   <section id="creer" class="bg-light-gray">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
@@ -99,7 +80,7 @@
             </div>
             </div>
         </div>
-    </section>
+    </section> -->
 
 <!-- ========================================================================================================================================== -->
 
@@ -122,176 +103,78 @@
     <section id="liste" class="bg-light-gray">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h1 class="section-heading">tableau chef : </h1>
 
-    <style>
-    @media (max-width: 500px) {
-    
-    .responsive-table-line td:before { content: attr(data-title); }
-    
-    .responsive-table-line table, 
-    .responsive-table-line thead, 
-    .responsive-table-line tbody, 
-    .responsive-table-line th, 
-    .responsive-table-line td, 
-    .responsive-table-line tr { 
-    display: block; 
-    }
-     
-    .responsive-table-line thead tr { 
-    display:none;
-    }
-    
-    .responsive-table-line td { 
-    position: relative;
-    border: 0px solid transparent;
-    padding-left: 50% !important; 
-    white-space: normal;
-    text-align:right; 
-    }
-     
-    .responsive-table-line td:before { 
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 45%; 
-    padding-right: 15px; 
-    height:100%;
-    white-space: nowrap;
-    text-overflow: ellipsis !important;
-    overflow:hidden !important;
-    text-align:left;
-    background-color:#f8f8f8;
-    padding:2px;
-    }
-    
-    }
-    </style>
-    
-    <div class="responsive-table-line" style="margin:0px auto;max-width:700px;">
-    <table class="table table-bordered table-condensed table-body-center" >
-    <thead>
-    <tr>
-    <th>Liste des projets</th>
-    <th>chef</th>
-    <th>fqdn</th>
-    <th>Cloud</th>
-    <th>modifier ou supprimer</th>
-    </tr>
-    </thead>
+            <p><h1 class="section-heading">VOS PROJETS</h1></p> <br>
 
-    <tbody>
-    <tr>
-    <td data-title="Liste des projets">projetX</td>
-    <td data-title="chef">#nom_du_chef</td>
-    <td data-title="fqdn"><a href="">projetX.codshare.itinet.fr</a></td>
-    <td data-title="Cloud"><a href="http://cloud.codshare.itinet.fr"><button type="button" class="btn btn-info">Cloud</button></a></td>
-    <td data-title="modifier ou supprimer"><a href="http://localhost/codshare/modifier.php"><button type="button" class="btn btn-warning">Modif.</button></a> <a href =""><button type="button" class="btn btn-danger">Supp.</button></a></td>
+            <h3 class="section-heading">tableau chef : </h3>
 
-    </tr>
-  
-
-     </tbody>
-    </table>
-    </div>
-
-
-
-
+             <table class="table table-striped">
+                <thead>
+                 <tr>
+                    <th>Liste des projets</th>
+                    <th>Chef</th>
+                    <th>fqdn</th>
+                    <th>Alias</th>
+                    <th>Cloud</th>
+                    <th>modifier ou supprimer</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td data-title="Liste des projets">projetX</td>
+                    <td data-title="chef">#nom_du_chef</td>
+                    <td data-title="fqdn"><a href="">projetX.codshare.itinet.fr</a></td>
+                    <td data-title="fqdn"><a href="">projetX</a></td>
+                    <td data-title="Cloud"><a href="http://cloud.codshare.itinet.fr"><button type="button" class="btn btn-info">Cloud</button></a></td>
+                    <td data-title="modifier ou supprimer"><a href="http://localhost/codshare/modifier.php"><button type="button" class="btn btn-warning">Modif.</button></a> <a href =""><button type="button" class="btn btn-danger">Supp.</button></a></td>
+                  </tr>
+                </tbody>
+              </table>
+              <a class="btn" href="http://localhost/codshare/creer.php" title="creer un projet"><button type="button" class="btn btn-success">+</button></a>
                 
             </div>
-            </div>
         </div>
-    </section>
+        
     
 
 <!-- ======================================================================================================================================================= -->
 
-
-        <!-- liste de projet Section -->
-    <section id="liste" class="bg-light-gray">
+<br><br>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h1 class="section-heading">tableau dev : </h1>
+             <h3 class="section-heading">tableau dev : </h1>
 
-    <style>
-    @media (max-width: 500px) {
-    
-    .responsive-table-line td:before { content: attr(data-title); }
-    
-    .responsive-table-line table, 
-    .responsive-table-line thead, 
-    .responsive-table-line tbody, 
-    .responsive-table-line th, 
-    .responsive-table-line td, 
-    .responsive-table-line tr { 
-    display: block; 
-    }
-     
-    .responsive-table-line thead tr { 
-    display:none;
-    }
-    
-    .responsive-table-line td { 
-    position: relative;
-    border: 0px solid transparent;
-    padding-left: 50% !important; 
-    white-space: normal;
-    text-align:right; 
-    }
-     
-    .responsive-table-line td:before { 
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 45%; 
-    padding-right: 15px; 
-    height:100%;
-    white-space: nowrap;
-    text-overflow: ellipsis !important;
-    overflow:hidden !important;
-    text-align:left;
-    background-color:#f8f8f8;
-    padding:2px;
-    }
-    
-    }
-    </style>
-    
-    <div class="responsive-table-line" style="margin:0px auto;max-width:700px;">
-    <table class="table table-bordered table-condensed table-body-center" >
-    <thead>
-    <tr>
-    <th>Liste des projets</th>
-    <th>chef</th>
-    <th>fqdn</th>
-    <th>Cloud</th>
-    </tr>
-    </thead>
+             <table class="table table-striped">
+                <thead>
 
-    <tbody>
-    <tr>
-    <td data-title="Liste des projets">projetX</td>
-    <td data-title="chef">#nom_du_chef</td>
-    <td data-title="fqdn"><a href="">projetX.codshare.itinet.fr</a></td>
-    <td data-title="Cloud"><a href="http://cloud.codshare.itinet.fr"><button type="button" class="btn btn-info">Cloud</button></a></td>
+                 <tr>
+                    <th>Liste des projets</th>
+                    <th>Chef</th>
+                    <th>fqdn</th>
+                    <th>Alias</th>
+                    <th>Cloud</th>
+                 </tr>
 
-    </tr>
-  
+                </thead>
+                <tbody>
 
-     </tbody>
-    </table>
-    </div>
+                  <tr>
+                    <td data-title="Liste des projets">projetX</td>
+                    <td data-title="chef">#nom_du_chef</td>
+                    <td data-title="fqdn"><a href="">projetX.codshare.itinet.fr</a></td>
+                    <td data-title="fqdn"><a href="">projetX</a></td>
+                    <td data-title="Cloud"><a href="http://cloud.codshare.itinet.fr"><button type="button" class="btn btn-info">Cloud</button></a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
 
 
 
 
                 
             </div>
-            </div>
-        </div>
     </section>
 
 
