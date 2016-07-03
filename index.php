93f7a24ccd86b696a32ebd8ca47b7c557616579c
hermann <nsiemo@intechinfo.fr>
2016-07-03 23:59:30 +0200

    modif

--- index.php
+++ index.php
@@ -58,9 +58,6 @@
                         <a class="page-scroll" href="#services">Offre des services</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#nosservices">Nos services</a>
-                    </li>
-                    <li>
                         <a class="page-scroll" href="#about">A propos</a>
                     </li>
                     <li>
@@ -127,107 +124,24 @@
                     <h4 class="service-heading">Stockage groupé</h4>
                     <p class="text-muted">Vous pouvez ainsi travailler en équipe en toute autonomie.</p>
                 </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Mail</h4>
+                    <p class="text-muted">Toutes les informations concernant votre boite mail.</p>
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+
+                </div>
             </div>
         </div>
     </section>
 
-    <!-- Portfolio Grid Section -->
-    <section id="nosservices" class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Nos services proposés</h2>
-                    <h3 class="section-subheading text-muted">Quelques apperçu du contenue</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/serveur-php-sql.png" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>Hébergement Web</h4>
-                        <p class="text-muted">Nous vous proposons de rendre en ligne votre site web.</p>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/Mail.png" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>Mail</h4>
-                        <p class="text-muted">Grâce a Rainloop, nous vous proposons une adresse mail unique.</p>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                  <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
-                  <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                      </div>
-                      <img src="img/portfolio/Cloud.png" alt="" width="263" height="256" class="img-responsive">
-                  </a>
-                    <div class="portfolio-caption">
-                        <h4>Cloud</h4>
-                        <p class="text-muted">Owncloud, la meilleur solution our vous permettre d'accéder a votre espace de stockage.</p>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/golden.png" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>Git</h4>
-                        <p class="text-muted">Nous avons choisi git pour que tout vos codes soient versionnés.</p>
-                    </div>
-                </div>
-<!--                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/escape.png" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>Escape</h4>
-                        <p class="text-muted">Website Design</p>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/dreams.png" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>Dreams</h4>
-                        <p class="text-muted">Website Design</p>
-                    </div>
--->                </div>
-            </div>
-        </div>
-    </section>
+
+
+
+
 
     <!-- About Section -->
     <section id="about">
